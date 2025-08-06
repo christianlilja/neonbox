964fc8dec5884c35875cf9409d5222da4b8018d2
Christian <crl@christianlilja.com>
2025-08-06 19:27:33 +0000

    added dtmf tone generator

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from boxes import bluebox
-from boxes import redbox
+from boxes import bluebox, redbox, dtmf
 
 def main():
-print("Phreaking Simulator")
+print("Color box Simulator")
 print("1. Bluebox")
 print("2. Redbox")
+print("3. DTMF Tone generator")
+
 choice = input("Select a tool: ")
 if choice == "1":
         while True:
@@ -20,6 +21,7 @@ if choice == "1":
                 bluebox.simulate_call_sequence()
             elif sub == "0":
                 break
+
 elif choice == "2":
     while True:
         print("\n[Redbox]")
@@ -34,6 +36,22 @@ elif choice == "2":
             redbox.simulate_coin("dime")
         elif sub == "3":
             redbox.simulate_coin("quarter")
+        elif sub == "0":
+            break
+
+elif choice == "3":
+    while True:
+        print("\n[DTMF Generator]")
+        print("1. Play single DTMF tone")
+        print("2. Play sequence")
+        print("0. Back")
+        sub = input("Choose an option: ")
+        if sub == "1":
+            key = input("Enter DTMF key (0-9, A-D, *, #): ")
+            dtmf.play_dtmf_tone(key)
+        elif sub == "2":
+            sequence = input("Enter key sequence (e.g. 18005551234): ")
+            dtmf.play_dtmf_sequence(sequence)
         elif sub == "0":
             break
 
